b6e49e587cf3090b9768c8474a4c8858f96f9c68
trulyslide <joeadk@gmail.com>
2023-12-10 09:13:46 -0500

    cleanup build

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,7 +1,7 @@
-const path = require(`path`)
+const path = require(`path`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   const result = await graphql(`
     query {
       allFile(filter: {extension: {in: ["js", "ts"]}}) {
@@ -12,12 +12,16 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `)
+  `);
 
   result.data.allFile.edges.forEach(({ node }) => {
+    // Remove the file extension from the path
+    const pathWithoutExtension = node.relativePath.replace(/\.[^/.]+$/, "");
+
     createPage({
-      path: node.relativePath,
+      path: pathWithoutExtension, // Use the modified path
       component: path.resolve(`./src/pages/${node.relativePath}`),
-    })
-  })
-}
+      // Include additional context if needed
+    });
+  });
+};
